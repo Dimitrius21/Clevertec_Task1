64c3f6b27fd14851bcca7899e96bfd9731751c34
Dmitri <ddd_2019@outlook.com>
2023-02-07 16:09:57 +0300

    add User entity and repo

---
+++ src/main/java/bzh/task1/dao/UserRepo.java
@@ -0,0 +1,10 @@
+package bzh.task1.dao;
+
+
+import bzh.task1.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+}
