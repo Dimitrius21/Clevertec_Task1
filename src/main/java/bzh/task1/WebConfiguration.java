49e53f3631ac146d7de428be090dd9e79deb6302
Dmitri <ddd_2019@outlook.com>
2023-02-07 15:50:48 +0300

    First commit - base

---
+++ src/main/java/bzh/task1/WebConfiguration.java
@@ -0,0 +1,18 @@
+package bzh.task1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("start");
+    }
+}
