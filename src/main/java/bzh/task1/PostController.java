a76872a5de474e3b647b3632c52807373f9c5bfd
Dmitri <ddd_2019@outlook.com>
2023-02-07 18:32:09 +0300

    change postController and Test for new functionality

--- src/main/java/bzh/task1/PostController.java
+++ src/main/java/bzh/task1/PostController.java
@@ -1,9 +1,14 @@
 package bzh.task1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/post")
 public class PostController {
+    @PostMapping
+    public void saveFile(){
+
+    }
 }
